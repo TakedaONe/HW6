8f00a4f8cbe2c0069c2f27068899b759d43a1721
TakedaONe <146793652+TakedaONe@users.noreply.github.com>
2024-05-20 23:46:36 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Weapon sword = new Weapon(WeaponType.SWORD, "BIGSWORD");
+
+        Boss boss = new Boss("BOSS", 1000, 150, sword);
+
+        boss.setHealth(1200);
+        boss.setDamage(50);
+
+        boss.printInfo();
     }
 }
